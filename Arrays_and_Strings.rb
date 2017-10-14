c7909d04ca4fa6b785a1f449d152f81e2c9816b2
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-10-13 20:32:00 -0400

    palindrome permutation

--- Arrays_and_Strings.rb
+++ Arrays_and_Strings.rb
@@ -13,4 +13,25 @@ end
 
 # urlify("Mr John Smith    ", 13)
 
+def palindrome_permutation(str)
+  char_hash = Hash.new(0)
+  alphanum_counter = 0
+
+  str.downcase.each_char do |char|
+    if ( char.ord >= 97 && char.ord <= 122 )
+      alphanum_counter += 1
+      char_hash[char] += 1
+    end
+  end
+
+  if alphanum_counter.even?
+    return false if char_hash.values.any?{ |num| num.odd? }
+  else
+    p char_hash.values.select { |num| num.odd? }
+    return false if char_hash.values.select { |num| num.odd? }.length.even?
+  end
+  true
+end
+
+# palindrome_permutation("Tact Coa")
 
