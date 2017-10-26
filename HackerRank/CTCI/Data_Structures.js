4ae143ab177c525631f58587c3850960b97e3f7a
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-10-25 21:49:53 -0400

    make anagram

--- HackerRank/CTCI/Data_Structures.js
+++ HackerRank/CTCI/Data_Structures.js
@@ -1,3 +1,4 @@
+// Left Rotation
 function main(k, n, a) {
     let rotations = k % n;
 
@@ -6,4 +7,45 @@ function main(k, n, a) {
         rotations -= 1;
     }
     console.log(a.toString().split(",").join(" "));
+}
+
+
+
+// Make Anagram
+function main() {
+    var a = readLine();
+    var b = readLine();
+
+    let arrA = a.split("");
+    let arrB = b.split("");
+    
+    
+    let store = {}
+    
+    arrA.forEach( letter => {
+        if ( !store[letter] ) {
+            store[letter] = 1
+        } else {
+            store[letter] -= 1
+        }
+    })
+    
+    arrB.forEach( letter => {
+        if ( !store[letter] ) {
+            store[letter] = 1
+        } else {
+            store[letter] -= 1
+        }
+    });
+    
+    arrS = Object.values(store);
+    
+    let counter = 0
+    arrS.forEach( num => {
+        if (num != 0) {
+            counter += Math.abs(num)
+        }
+    });
+    
+    return counter
 }
