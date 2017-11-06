92846edfaafc17d39d5b07aae9ea8be5323629ec
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-11-05 21:05:38 -0500

    trim binary tree

--- Leetcode/Trees.rb
+++ Leetcode/Trees.rb
@@ -1,3 +1,4 @@
+# Merge Two Binary Trees
 def merge_trees(t1, t2)
     return t2 if t1 == nil
     return t1 if t2 == nil
@@ -6,4 +7,20 @@ def merge_trees(t1, t2)
     t1.left = merge_trees(t1.left, t2.left)
     t1.right = merge_trees(t1.right, t2.right)
     t1
+end
+
+# Trim a Binary Search Tree
+def trim_bst(root, l, r)
+    return nil if root == nil 
+    
+    if root.val < l
+        root = trim_bst(root.right, l, r)
+    elsif root.val > r
+        root = trim_bst(root.left, l, r)
+    else
+        root.left = trim_bst(root.left, l, r)
+        root.right = trim_bst(root.right, l, r)
+    end
+    p root
+    root
 end
