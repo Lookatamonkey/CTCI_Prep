5b692da70618c209eccfb6d36e0893fe0428ca71
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-12-09 21:42:50 -0500

    finish nth prime

--- AA/practice-problems/problems/15-is-prime.rb
+++ AA/practice-problems/problems/15-is-prime.rb
@@ -13,6 +13,15 @@
 # Difficulty: medium.
 
 def is_prime?(number)
+    return false if number <= 1 
+
+    i = 2
+    while i < number
+        return false if number % i == 0
+        i += 1
+    end
+
+    true
 end
 
 # These are tests to check that your code is working. After writing
