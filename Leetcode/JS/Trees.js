e6babc1bcc29a19869c9573ee81506177a430817
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-02-10 17:45:54 -0500

    same tree

--- Leetcode/JS/Trees.js
+++ Leetcode/JS/Trees.js
@@ -30,3 +30,12 @@ var inverter = function(root) {
     root.left = root.right;
     root.right = temp;
 };
+
+// 100 - Same Tree
+var isSameTree = function(p, q) {
+    if ((p === null && q !== null) || (q === null && p !== null)) { return false; }
+    if (p === null & q === null) { return true; }
+    if (p.val !== q.val) { return false; }
+    
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
