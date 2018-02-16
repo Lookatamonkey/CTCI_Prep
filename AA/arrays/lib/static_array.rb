ebd0a73d1f915dfd0c741deb42fe95f898c65445
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-02-15 23:21:30 -0500

    add static array

--- AA/arrays/lib/static_array.rb
+++ AA/arrays/lib/static_array.rb
@@ -1,14 +1,28 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
+  attr_reader :lengthst
+
   def initialize(length)
+    @length = length
+    @store = Array.new(length)
   end
 
   # O(1)
   def [](index)
+    if index + 1 > @length
+      raise 'index out of bounds'
+    else
+      @store[index]
+    end
   end
 
   # O(1)
   def []=(index, value)
+    if index + 1 > @length
+      raise 'index out of bounds'
+    else
+      @store[index] = value
+    end
   end
 
   protected
