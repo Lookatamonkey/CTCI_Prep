bbe1964d7066da9ec0cc3c644c9fd12cbf06c17d
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-02-16 21:07:50 -0500

    finish hashset

--- AA/hash-map/lib/p03_hash_set.rb
+++ AA/hash-map/lib/p03_hash_set.rb
@@ -9,18 +9,34 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count == @store.length
+    hashed = key.hash
+    self[key] << hashed
+    @count += 1
   end
 
   def include?(key)
+    hashed = key.hash
+    i = 0
+    while i < self[key].length
+      return true if self[key][i] == hashed
+      i += 1
+    end
+    false
   end
 
   def remove(key)
+    if include?(key)
+      hashed = key.hash
+      self[key].delete(hashed)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +44,16 @@ class HashSet
   end
 
   def resize!
+    temp_store = Array.new(num_buckets * 2)  { [] }
+    i = 0
+    while i < @store.length
+      j = 0
+      while j < @store[i].length
+        el = @store[i][j]
+        temp_store[el % (num_buckets * 2)]
+        j += 1
+      end
+      i += 1
+    end  
   end
 end
