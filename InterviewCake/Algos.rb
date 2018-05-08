99c0f48eb36da355a3ca718170f4648651fb1a7b
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-05-07 20:03:04 -0400

    max duffel need to come back to this

--- InterviewCake/Algos.rb
+++ InterviewCake/Algos.rb
@@ -353,3 +353,72 @@ def iterative_fibs(num)
 end
 
 # p iterative_fibs(10)
+
+# def max_duffel_bag_value(cake_arr, capacity)
+#     total = 0
+
+#     cake_arr.select! { |cake| cake[0] != 0 }
+
+#     p cake_arr
+#     cost_per_pound = cake_arr.sort do |x, y|
+#         (y[1] / y[0]) <=> (x[1] / x[0])
+#     end
+
+#     cost_per_pound.each do |cake|
+#         weight = cake[0]
+#         value = cake[1]
+#         next if weight == 0
+#         if (capacity / weight) >= 0
+#             total += (capacity / weight) * value
+#             capacity %= weight
+#         end
+#     end
+
+#     total
+# end
+
+def max_duffel_bag_value(cake_arrays, weight_capacity)
+
+    # we make an array to hold the maximum possible value at every
+    # duffel bag weight capacity from 0 to weight_capacity
+    # starting each index with value 0
+    max_values_at_capacities = [0] * (weight_capacity + 1)
+  
+    (0..weight_capacity).each do |current_capacity|
+  
+      # set a variable to hold the max monetary value so far for current_capacity
+      current_max_value = 0
+        
+      cake_arrays.each do |cake_weight, cake_value|
+
+        # if a cake weighs 0 and has a positive value the value of our duffel bag is infinite!
+        if (cake_weight == 0 && cake_value != 0)
+          return Float::INFINITY
+        end
+  
+        # if the current cake weighs as much or less than the current weight capacity
+        # it's possible taking the cake would get a better value
+        if (cake_weight <= current_capacity)
+  
+          # so we check: should we use the cake or not?
+          # if we use the cake, the most kilograms we can include in addition to the cake
+          # we're adding is the current capacity minus the cake's weight. we find the max
+          # value at that integer capacity in our array max_values_at_capacities
+          p [current_capacity, cake_weight], max_values_at_capacities
+          remaining_capacity = current_capacity - cake_weight
+           max_value_using_cake = cake_value + max_values_at_capacities[remaining_capacity]
+        #    p max_values_at_capacities
+          # now we see if it's worth taking the cake. how does the
+          # value with the cake compare to the current_max_value?
+          current_max_value = [max_value_using_cake, current_max_value].max
+        end
+      end
+  
+      # add each capacity's max value to our array so we can use them
+      # when calculating all the remaining capacities
+      max_values_at_capacities[current_capacity] = current_max_value
+    end
+    return max_values_at_capacities[weight_capacity]
+  end
+
+p max_duffel_bag_value([[3, 40], [5, 70]], 9)
