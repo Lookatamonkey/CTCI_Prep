7ed8700ed912fdc65d91dff4bf70b4209b987e2f
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-07-09 01:45:23 -0400

    search 2d matrix II

--- Leetcode/Ruby/Algos.rb
+++ Leetcode/Ruby/Algos.rb
@@ -1453,3 +1453,24 @@ end
 
 # p generate_parenthesis(3)
 
+# 240. Search a 2D Matrix II
+
+def search_matrix(matrix, target)
+    row = matrix.length - 1
+    col = 0
+
+    until row >= matrix.length || col >= matrix[0].length || row < 0 || col < 0
+        p num = matrix[row][col]
+
+        return true if num == target
+        col += 1 if num < target
+        row -= 1 if num > target
+    end
+
+    false
+end
+
+p search_matrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+    5)
+
+
